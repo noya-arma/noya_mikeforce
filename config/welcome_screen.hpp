bf0e90a43f5546ba1493e0e7cef99b0cec3251e7
shocho <noya@ruu.kr>
2023-07-08 03:07:06 +0900

    웰컴 변경

--- config/welcome_screen.hpp
+++ config/welcome_screen.hpp
@@ -10,7 +10,7 @@ class ParaWelcomeScreen {
 			"<t align='center'>",
 			"<t align='center' size='3' color='FF0000'>시작 후 반드시 'H' 버튼을 눌러 자신의 소속 팀 을 설정해주세요!</t>",
 			"<br/><br/>",
-			"현재 미션은 한글번역 및 크고 작은 수정이 포함되어 있습니다. 재미있게 즐겨주세요.",
+			"NOYA 서버 주 플레이 시간 : 평일 오후 8시 / 주말 오후 2시",
 			"<br/><br/>",
 			"마이크포스 외에 다른 ARMA 작전도 함께 하고싶다면 <a href='http://discord.gg/CCWcySuM3b'>디스코드</a>를 방문해주세요",
 			"<br/><br/>",
