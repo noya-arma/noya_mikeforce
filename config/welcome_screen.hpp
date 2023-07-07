30a7bd6078ccc4cee603dccfe192a04fd430cbb5
shocho <noya@ruu.kr>
2023-07-08 03:07:49 +0900

    웰컴 변경

--- config/welcome_screen.hpp
+++ config/welcome_screen.hpp
@@ -5,12 +5,12 @@ class ParaWelcomeScreen {
         title = "Welcome to Mike Force";
         /*content = "Structured Text Content";*/
 		content[] = {
-			"<t align='center' size='1.5'>NOYA 서버에 오신걸 환영합니다!</t>",
+			"<t align='center' size='1'>NOYA 서버에 오신걸 환영합니다!</t>",
 			"<br/>",
 			"<t align='center'>",
-			"<t align='center' size='3' color='FF0000'>시작 후 반드시 'H' 버튼을 눌러 자신의 소속 팀 을 설정해주세요!</t>",
+			"<t align='center' size='2' >시작 후 반드시 'H' 버튼을 눌러 자신의 소속 팀 을 설정해주세요!</t>",
 			"<br/><br/>",
-			"NOYA 서버 주 플레이 시간 : 평일 오후 8시 / 주말 오후 2시",
+			"<t align='center' size='2' >NOYA 서버 주 플레이 시간 : 평일 오후 8시 / 주말 오후 2시</t>",
 			"<br/><br/>",
 			"마이크포스 외에 다른 ARMA 작전도 함께 하고싶다면 <a href='http://discord.gg/CCWcySuM3b'>디스코드</a>를 방문해주세요",
 			"<br/><br/>",
